2e4d67fa9fc4be10471c442b9046542433d6ad21
vladho <vladhopilo@gmail.com>
2021-02-19 14:41:02 +0200

    fix

--- src/components/FriendList/SingleFriend/SingleFriend.js
+++ src/components/FriendList/SingleFriend/SingleFriend.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './SingleFriend.module.css';
+import {
+  item,
+  online,
+  offline,
+  stylesAvatar,
+  stylesName,
+} from './SingleFriend.module.css';
 
 const SingleFriend = props => {
   const { avatar, name, isOnline } = props;
 
   return (
-    <li className={styles.item}>
-      {isOnline ? (
-        <span className={styles.online}></span>
-      ) : (
-        <span className={styles.offline}></span>
-      )}
-      {/* <span className={isOnline ? `{styles.online}` : 'offline'}></span> */}
-      <img className={styles.avatar} src={avatar} alt="" width="48" />
-      <p className={styles.name}>{name}</p>
+    <li className={item}>
+      <span className={isOnline ? online : offline}></span>
+      <img className={stylesAvatar} src={avatar} alt="" width="48" />
+      <p className={stylesName}>{name}</p>
     </li>
   );
 };
--- src/components/TransactionHistory/SingleTransaction/SingleTransaction.js
+++ src/components/TransactionHistory/SingleTransaction/SingleTransaction.js
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types';
 
 const SingleTransaction = props => {
   const { type, amount, currency } = props;
-  // console.log(props);
+
   return (
-    <>
-      <tr>
-        <td>{type}</td>
-        <td>{amount}</td>
-        <td>{currency}</td>
-      </tr>
-    </>
+    <tr>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
+    </tr>
   );
 };
 
